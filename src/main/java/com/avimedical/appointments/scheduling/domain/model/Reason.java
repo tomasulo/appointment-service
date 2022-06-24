c3dc9f04e29857769e75d020f07749afc98783d7
Thomas Kieffer <tomasulo@tuta.io>
2022-06-24 13:41:14 +0200

    Remove unnecessary getters and setters

--- src/main/java/com/avimedical/appointments/scheduling/domain/model/Reason.java
+++ src/main/java/com/avimedical/appointments/scheduling/domain/model/Reason.java
@@ -16,20 +16,4 @@ public class Reason {
         // TODO
         return Duration.ofMinutes(30);
     }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
